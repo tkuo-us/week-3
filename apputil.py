defd920367b0aa5f22fff09128470f6971838225
tkuo-us <tkuo@iu.edu>
2025-09-18 19:55:03 +0000

    fix DataFrame

--- apputil.py
+++ apputil.py
@@ -24,14 +24,21 @@ def to_binary(n):
         return to_binary(n // 2) + str(n % 2)
      
 
-def task_1(df: pd.DataFrame):
+url = 'https://github.com/melaniewalsh/Intro-Cultural-Analytics/raw/master/book/data/bellevue_almshouse_modified.csv'
+df = pd.read_csv(url)
+
+df["date_in"] = pd.to_datetime(df["date_in"], errors="coerce")
+df["year"] = df["date_in"].dt.year
+
+def task_1():
     """
     Return a list of all column names in df_bellevue
     Sorted by the number of NA values in each column, from fewest NA values to most NA values.
     """
     return df.isna().sum().sort_values().index.tolist()
 
-def task_2(df: pd.DataFrame):
+
+def task_2():
     """
     Return total number of entries for each year：
     - year
@@ -48,7 +55,7 @@ def task_2(df: pd.DataFrame):
     )
     return output
 
-def task_3(df: pd.DataFrame):
+def task_3():
     """
     Return the average age of genders "m" and "w"
     as a Series indexed by
@@ -59,7 +66,7 @@ def task_3(df: pd.DataFrame):
     tmp.loc[~tmp["gender"].isin(["m", "w"]), "gender"] = pd.NA # only keep "m" and "w", set others to NA
     return tmp.groupby("gender")["age"].mean()
 
-def task_4(df: pd.DataFrame):
+def task_4():
     """
     Return TOP 5 profession
     """
